43eea4117a1353d22da51d11d3ef7d11c9e3fb65
Jack Madden <jck13mad@gmail.com>
2020-01-25 20:30:06 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  students = new.Array
+  students = ["Timmy", "Jimmy", "Jimmy Again", "Kevin"]
 end
 
 def add_element_to_end_of_array(array, element)
